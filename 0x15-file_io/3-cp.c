8ec4df66aec5337b5ddfa5f9f8d1050e5a62968a
Oluwatobi <amure387@gmail.com>
2022-10-31 10:33:52 +0100

    push

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,38 +1,38 @@
 #include "main.h"
 void check_valid(int val, char *filename, mode)
 {
-    if (val == -1 && mode == 'O')
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
-        exit(98);
-    }
-    else if (val == -1 && mode == 'W')
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
-        exit(99);
-    }
-    else if (val == -1 && mode == 'C')
-    {
-        dprintf(STDERR_FILENO, "Can't close fd %i", val);
-        exit(100);
-    }
+	if (val == -1 && mode == 'O')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
+		exit(98);
+	}
+	else if (val == -1 && mode == 'W')
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
+		exit(99);
+	}
+	else if (val == -1 && mode == 'C')
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %i", val);
+		exit(100);
+	}
 }
 int main(int count, char **value)
 {
-    int file_fro, file_to, c_read = 1024, c_written;
-    char buf[1024];
+	int file_fro, file_to, c_read = 1024, c_written;
+	char buf[1024];
 
-    if (count != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-    }
+	if (count != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+	}
 
-    file_fro = open(value[1], O_RDONLY | O_CREAT | O_TRUNC);
-    check_valid(file_fro, value[1], 'O');
-    file_to = open(value[2], O_RDONLY | O_CREAT | O_TRUNC);
-    check_valid(file_to, value[2], 'O');
-    while (c_read == 1024)
-    {
-        
-    }
+	file_fro = open(value[1], O_RDONLY | O_CREAT | O_TRUNC);
+	check_valid(file_fro, value[1], 'O');
+	file_to = open(value[2], O_RDONLY | O_CREAT | O_TRUNC);
+	check_valid(file_to, value[2], 'O');
+	while (c_read == 1024)
+	{
+
+	}
 }
